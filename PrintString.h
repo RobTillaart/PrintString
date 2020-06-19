349432e6f2eb2c45c011e48e92eac93fa3250e2d
rob tillaart <rob.tillaart@gmail.com>
2020-06-19 16:30:38 +0200

    fix library.json

--- PrintString.h
+++ PrintString.h
@@ -2,19 +2,20 @@
 //
 //    FILE: PrintString.h
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.1.1
+// VERSION: 0.1.2
 // PURPOSE: Class that captures prints into a String 
 //    DATE: 2017-12-09
 //     URL: https://github.com/RobTillaart/PrintString
 //
 // 0.1.0    2017-12-09 initial version
 // 0.1.1    2020-04-26 added size
+// 0.1.2    2020-06-19 fix library.json
 //
 
 #include "Print.h"
 #include "String.h"
 
-#define PRINTSTRING_VERSION "0.1.1"
+#define PRINTSTRING_VERSION "0.1.2"
 
 class PrintString: public Print
 {
